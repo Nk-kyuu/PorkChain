c8215586cb961bbcd6fa36e0f1de361af8a851fa
IngkamonCH <namkhing1830@gmail.com>
2024-02-27 19:45:42 +0700

    second push

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,12 @@
+import './login.css';
+
+const login = () => {
+    return (
+        <div className="box">
+            <h1>This is login page</h1>
+        </div>
+        
+    );
+}
+
+export default login;
